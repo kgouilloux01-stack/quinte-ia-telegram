63a81e3497d3858aa82e838e0d0514cfe3a11eb5
LEBOTEUR <kgouilloux01@gmail.com>
2026-01-11 14:35:15 +0000

    Update bot_github.py

--- bot_github.py
+++ bot_github.py
@@ -152,7 +152,7 @@ def main():
 
                 delta_min = int((heure_course - now).total_seconds() / 60)
                 # ✅ Intervalle 10-15 min avant le départ
-                if 10 <= delta_min <= 15:
+                if delta_min >= 0:  # toutes les courses futures
                     allocation, distance, partants, chevaux = get_course_detail(link)
                     if not chevaux:
                         continue
